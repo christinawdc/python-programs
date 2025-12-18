03a5d62d4d2e5507a152b9b401fa40b8c2eb4ea2
christinawdc <christinawdc@gmail.com>
2025-12-18 23:55:50 +0530

    Updated print.py

--- basics/print.py
+++ basics/print.py
@@ -4,6 +4,7 @@ print ("Hello World!")
 
 # You can print multiple expressions, separated by commas:
 print ("Hello", "World", "!")
+print ("Sum of 2 and 3 is", 2 + 3) #Numbers can be printed without double quotes. They can be calculated directly.
 
 # In the above example, the output is automatically separated by a space.
 # You can change the separator using the 'sep' parameter:
