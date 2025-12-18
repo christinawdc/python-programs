73e1f288431b0e8354d8bac9db956d7ee757adf6
christinawdc <christinawdc@gmail.com>
2025-12-18 23:52:45 +0530

    Added print.py

---
+++ basics/print.py
@@ -0,0 +1,15 @@
+# The syntax for print is:
+# print (<expression>)
+print ("Hello World!")
+
+# You can print multiple expressions, separated by commas:
+print ("Hello", "World", "!")
+
+# In the above example, the output is automatically separated by a space.
+# You can change the separator using the 'sep' parameter:
+print ("Hello", "World", "!", sep="-")
+print ("2024", "06", "15", sep="/")
+# You can also change the end character using the 'end' parameter:
+print ("Hello", end="***")
+print ("Goodbye", end="") # No newline at the end
+
